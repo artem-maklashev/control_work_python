3fb166aa71d2f864325568f8423c7d333e0b362a
Маклашев Артем <maklashev_ak@outlook.com>
2023-03-30 23:20:40 +0400

    test for json parse

--- notes/Notes.py
+++ notes/Notes.py
@@ -2,10 +2,12 @@ from notes.data import Note
 
 
 class Notes:
-    __notes_list: list[Note] = []
+    # __notes_list: list[Note] = []
 
-    def __init__(self):
-        self.__notes_list: list[Note] = []
+    def __init__(self, notes_list=None):
+        if notes_list is None:
+            notes_list: list[Note] = []
+        self.__notes_list = notes_list
 
     def add__note(self, my__note: Note):
         self.__notes_list.append(my__note)
@@ -16,4 +18,27 @@ class Notes:
     def get__note(self, index: int):
         return self.__notes_list[index]
 
+    @property
+    def notes(self):
+        return self.__notes_list
 
+    def to_dict(self):
+        my_dict = {}
+        for note in self.__notes_list:
+            my_dict[note.get_id] = note.__dict__
+        return my_dict
+
+
+    @staticmethod
+    def to_notes(data):
+        notes_list = []
+        for item in data:
+            note = Note()
+            note.id = item["_Note__id"]
+            note.c_date = item["_Note__note_creation_date"]
+            note.text = item["_Note__note_text"]
+            note.e_date = item["_Note__note_creation_date"]
+            note.header = item["_Note__note_header"]
+            notes_list.append(note)
+        notes = Notes(notes_list)
+        return notes
--- notes/data/Note.py
+++ notes/data/Note.py
@@ -9,8 +9,40 @@ class Note:
         self.__id = next(Note.__note_id)
         self.__note_header = header
         self.__note_text = text
-        self.__note_creation_date = datetime.now()
-        self.__note_edit_date = None
+        self.__note_creation_date = datetime.now().strftime("%B %d, %Y")
+        self.__note_edit_date = datetime.now().strftime("%B %d, %Y")
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    @property
+    def text(self):
+        return self.__note_text
+
+    @text.setter
+    def text(self, value):
+        self.__note_text = value
+
+    @property
+    def c_date(self):
+        return self.__note_creation_date
+
+    @c_date.setter
+    def c_date(self, value):
+        self.__note_creation_date = value
+
+    @property
+    def e_date(self):
+        return self.__note_edit_date
+
+    @e_date.setter
+    def e_date(self, value):
+        self.__note_edit_date = value
 
     def __str__(self):
         return (f"id:\t\t {self.__id}\n"
@@ -51,4 +83,10 @@ class Note:
     def edit_date(self, edit_date):
         self.__note_edit_date = edit_date
 
-
+    # @staticmethod
+    # def from_json(json_dct):
+    #     return Note(json_dct['_Note__id'],
+    #                 json_dct['_Note__note_creation_date'],
+    #                 json_dct['_Note__note_edit_date'],
+    #                 json_dct['_Note__note_header'],
+    #                 json_dct['_Note__note_text'])
